86be8b6a633e23fe477d7e47aac8d34ff9dbe277
Mathias Mohn Mørch <mathmoe64@gmail.com>
2023-12-12 16:35:28 +0100

    chore: Removed ECS scripts

--- Assets/Script/CelestialMassComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Unity.Entities;
-using UnityEngine;
-
-namespace Script {
-    public struct CelestialMassComponent : IComponentData {
-        [Tooltip("Unit - Solar Mass SM 2 * 10^30")] 
-        public float celestialMass;
-    }
-}
--- Assets/Script/CelestialVelocityComponent.cs
+++
@@ -1,8 +0,0 @@
-﻿using Unity.Entities;
-using Unity.Mathematics;
-
-namespace Script {
-    public struct CelestialVelocityComponent : IComponentData {
-        public float3 celestialVelocity;
-    }
-}
