f1465113ddb3050a2bb6a7539b304289c7e7e4ad
Mathias Mohn Mørch <mathmoe64@gmail.com>
2023-12-12 16:26:50 +0100

    feat: Added asteroid belt

---
+++ Assets/Script/AsteorideBelt/AsteroidBelt.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AsteroidBelt : MonoBehaviour {
+    
+    [SerializeField] private GameObject asteroidPrefab;
+    
+    [SerializeField] private int asteroidCount = 100;
+    [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float beltRotationSpeed = 1f;
+    [SerializeField] private float innerSize = 2f;
+    [SerializeField] private float outerSize = 5f;
+
+    private List<Transform> asteroids = new List<Transform>();
+
+    private void Start() {
+        float stepAngles = 360f / (float)asteroidCount;
+        for (int i = 0; i < asteroidCount; i++) {
+            Vector3 position = transform.position + Quaternion.Euler(0, stepAngles * i, 0) * Vector3.forward * UnityEngine.Random.Range(innerSize, outerSize);
+            Transform trans = Instantiate(asteroidPrefab, position, Random.rotation, transform).transform;
+            trans.localScale *= Random.Range(0.04f, 0.2f);
+            asteroids.Add(trans);
+        }
+    }
+
+
+    private void Update() {
+        Vector3 rot = transform.localEulerAngles;
+        rot.y += beltRotationSpeed * Time.deltaTime;
+        transform.localEulerAngles = rot;
+        
+        foreach (var asteroid in asteroids) {
+            asteroid.rotation *= Quaternion.Euler(0, rotateSpeed * Time.deltaTime, 0) ;
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Handles.DrawWireArc(transform.position, transform.up, transform.right, 360, innerSize, 3f);
+        Handles.DrawWireArc(transform.position, transform.up, transform.right, 360, outerSize, 3f);
+    }
+
+    private void OnValidate() {
+        if (innerSize < 0) {
+            innerSize = 0f;
+        }
+        if (outerSize < innerSize) {
+            outerSize = innerSize;
+        }
+    }
+}
